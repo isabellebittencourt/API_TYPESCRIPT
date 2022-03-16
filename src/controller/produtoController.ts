0cff9675992b96bb89979a3580930abf6832fad3
Isabelle Bittencourt <69654889+isabellebittencourt@users.noreply.github.com>
2022-03-15 21:26:59 -0300

    creating model

--- src/controller/produtoController.ts
+++ src/controller/produtoController.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express";
+import { produto } from "../models/produto";
+
+const produtos : produto[] = []
 
 
 
 export class produtoController{
     create(request: Request, response: Response){
+        produtos.push(request.body)
         response.status(201).json({message: "Produto cadastrado"})
+
     }
 
     list(){}
---
+++ src/models/produto.ts
@@ -0,0 +1,7 @@
+
+export interface produto{
+    nome: String,
+    descricao: String,
+    quantidade: number,
+    preco: number
+}
