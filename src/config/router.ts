727a6ad7d53608d4b4ff29965c8609805d0ddd93
Isabelle Bittencourt <69654889+isabellebittencourt@users.noreply.github.com>
2022-03-15 21:16:48 -0300

    new files and updates

--- src/config/router.ts
+++ src/config/router.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
+import { produtoController } from "../controller/produtoController";
 
 const routes = Router()
+
 routes.get("/", (req, res)=>{
     res.json({message: "Hello world"})
 })
+
+routes.post("/produto/create", new produtoController().create)
+routes.get("/produto/list", new produtoController().list)
+routes.put("/produto/update", new produtoController().update)
+routes.delete("/produto/delete", new produtoController().delete)
+
 
 export {routes};
--- src/controller/produtoController.ts
+++ src/controller/produtoController.ts
@@ -1,1 +1,16 @@
-export class produtoController{}
+import { Request, Response } from "express";
+
+
+
+export class produtoController{
+    create(request: Request, response: Response){
+        response.status(201).json({message: "Produto cadastrado"})
+    }
+
+    list(){}
+    
+    update(){}
+    
+    delete(){}
+
+}
