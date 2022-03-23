11b4779a69a989014dc7c2ed8fa9107a24ae38bf
Isabelle Bittencourt <69654889+isabellebittencourt@users.noreply.github.com>
2022-03-22 21:21:25 -0300

    change line json

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import { routes } from './config/router';
 
 const app = express();
-
-app.use(routes);
 app.use(express.json());
+app.use(routes);
+
 app.listen(3333, () => {
     console.log('Servidor rodando')
 
