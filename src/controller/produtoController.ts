951fa90a3a097cbd7725953064837ac63fc10a01
Isabelle Andrade <isabelleandrade164@gmail.com>
2022-03-29 21:00:21 -0300

    updates

--- src/controller/produtoController.ts
+++ src/controller/produtoController.ts
@@ -1,13 +1,26 @@
 import { Request, Response } from "express";
 import { produtoRepository } from "../repositories/produtoRepository";
-
+import axios, { Axios } from "axios";
 const produtoRespo = new produtoRepository();
 
 export class produtoController{
 
-      create(request:Request, response: Response) : void{
+    create(request:Request, response: Response) : void{
+        const produto = request.body;
         const produtos = produtoRespo.create(request.body);
+        
+
+        axios.post("http://localhost:3334/produto/cadastrar",produto)
+        .then((response) => {
+          console.log(response)  
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+        
         response.status(201).json({message : "produto cadastrado", data: produtos},)
+
+
     }
 
     list(request:Request, response:Response) {
@@ -16,8 +29,6 @@ export class produtoController{
 
     }
     
-
-
     update(request:Request, response:Response){
         const produtos = produtoRespo.update(request.body)
         response.status(200).json({message : "Produto alterado", data: produtos})
